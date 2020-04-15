c7c68eb4cb6ac50aca5ed9760f42135b79612dd0
Sergey <dushechkin_serge@mail.ru>
2020-04-15 17:55:08 +0300

    Добавил константы, словарь с предлогами к типу события

---
+++ src/const.js
@@ -0,0 +1,14 @@
+export const EVENT_TYPES = [`taxi`, `bus`, `train`, `ship`, `transport`, `drive`, `flight`, `check`, `sightseeing`, `restaurant`];
+
+export const EVENT_PREP = {
+  [`taxi`]: `to`,
+  [`bus`]: `to`,
+  [`train`]: `to`,
+  [`ship`]: `to`,
+  [`transport`]: `to`,
+  [`drive`]: `to`,
+  [`flight`]: `to`,
+  [`check`]: `in`,
+  [`sightseeing`]: `in`,
+  [`restaurant`]: `in`
+};
