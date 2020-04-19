0c658fd9930b3d1b8b36a1042079f81e4268666e
Sergey <54816584+sergeydushechkin@users.noreply.github.com>
2020-04-19 13:42:56 +0300

    Добавлена отрисовка цены путешествия

--- src/components/cost.js
+++ src/components/cost.js
@@ -1,8 +1,15 @@
-export const createTripCostTemplate = () => {
+export const createTripCostTemplate = (events) => {
+  const cost = events
+    .reduce((totalCost, event) => {
+      return totalCost + event.price + event.offers.reduce((totalOffersPrice, offer) => {
+        return totalOffersPrice + (offer.checked ? offer.price : 0);
+      }, 0);
+    }, 0);
+
   return (
     `<section class="trip-main__trip-info  trip-info">
       <p class="trip-info__cost">
-        Total: &euro;&nbsp;<span class="trip-info__cost-value">0</span>
+        Total: &euro;&nbsp;<span class="trip-info__cost-value">${cost}</span>
       </p>
     </section>`
   );
