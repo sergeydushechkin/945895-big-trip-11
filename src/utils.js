df5306a7d32dbd2cb68f64327f1486dcddf419e9
Sergey <dushechkin_serge@mail.ru>
2020-04-21 17:55:32 +0300

    Добавляет два способа добавления элементов в разметку

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+  BEFOREBEGIN: `beforebegin`,
+  AFTEREND: `afterend`
+};
+
 export const getRandomArrayElement = (array) =>
   array[Math.floor(Math.random() * array.length)];
 
@@ -57,4 +64,21 @@ export const createElement = (elementTemplate) => {
   const tempElement = document.createElement(`div`);
   tempElement.innerHTML = elementTemplate;
   return tempElement.firstChild;
+};
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
 };
