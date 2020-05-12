a60b5d4da90b08b9b0ac287cdb754ea633a76657
Sergey <dushechkin_serge@mail.ru>
2020-05-12 18:00:25 +0300

    Добавляет в проект использование библиотеки moment

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export const getRandomArrayElement = (array) =>
   array[Math.floor(Math.random() * array.length)];
 
@@ -8,27 +10,15 @@ const castTimeFormat = (value) =>
   value < 10 ? `0${value}` : String(value);
 
 export const formatTime = (date) => {
-  const hours = castTimeFormat(date.getHours());
-  const minutes = castTimeFormat(date.getMinutes());
-
-  return `${hours}:${minutes}`;
+  return moment(date).format(`hh:mm`);
 };
 
 export const formatFullDate = (date) => {
-  const day = castTimeFormat(date.getDate());
-  const month = castTimeFormat(date.getMonth() + 1);
-  const year = String(date.getFullYear()).slice(2);
-  const hours = castTimeFormat(date.getHours());
-  const minutes = castTimeFormat(date.getMinutes());
-
-  return `${day}/${month}/${year} ${hours}:${minutes}`;
+  return moment(date).format(`DD/MM/YYYY HH:mm`);
 };
 
 export const formatDateReverse = (date) => {
-  const day = castTimeFormat(date.getDate());
-  const month = castTimeFormat(date.getMonth() + 1);
-  const year = date.getFullYear();
-  return `${year}-${month}-${day}`;
+  return moment(date).format(`YYYY-MM-DD`);
 };
 
 const formatDurationTime = (date) => {
@@ -38,16 +28,25 @@ const formatDurationTime = (date) => {
   return `${hours}H ${minutes}M`;
 };
 
-export const formatDuration = (date) => {
-  let duration = ``;
-  if (date >= 86400000) {
-    duration = `${castTimeFormat(Math.floor(date / 1000 / 60 / 60 / 24))}D ${formatDurationTime(date)}`;
-  } else if (date >= 3600000) {
-    duration = `${formatDurationTime(date)}`;
-  } else {
-    duration = `${Math.round(date / 1000 / 60)}M`;
-  }
-  return duration;
+export const formatDuration = (dateFirst, dateSecond) => {
+  // let duration = ``;
+  // const date = new Date(dateSecond - dateFirst);
+
+  // if (date >= 86400000) {
+  //   duration = `${castTimeFormat(Math.floor(date / 1000 / 60 / 60 / 24))}D ${formatDurationTime(date)}`;
+  // } else if (date >= 3600000) {
+  //   duration = `${formatDurationTime(date)}`;
+  // } else {
+  //   duration = `${Math.round(date / 1000 / 60)}M`;
+  // }
+  // return duration;
+
+  const duration = moment.duration(moment(dateSecond).diff(dateFirst));
+
+  const days = Math.floor(duration.asDays());
+  const hours = Math.floor(duration.asHours()) - days * 24;
+  const minutes = Math.floor(duration.asMinutes()) - hours * 60;
+  return `${days}D ${hours}H ${minutes}M`;
 };
 
 export const capitalizeFirstLetter = (string) =>
