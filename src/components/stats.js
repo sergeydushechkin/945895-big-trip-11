44b1f00e6ba69186d2c5b2c36fc6bde567023462
Sergey <dushechkin_serge@mail.ru>
2020-05-19 17:57:08 +0300

    Реализация отрисовки статистики 1

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,4 +1,8 @@
 import AbstractSmartComponent from "./abstract-smart-component.js";
+import Chart from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+const BAR_HEIGHT = 55;
 
 const createStatsTemplate = () => {
   return (
@@ -23,5 +27,17 @@ const createStatsTemplate = () => {
 export default class StatsComponents extends AbstractSmartComponent {
   getTemplate() {
     return createStatsTemplate();
+  }
+
+  _renderCharts() {
+    const moneyCtx = document.querySelector(`.statistic__money`);
+    const transportCtx = document.querySelector(`.statistic__transport`);
+    const timeSpendCtx = document.querySelector(`.statistic__time-spend`);
+
+    moneyCtx.height = BAR_HEIGHT * 6;
+    transportCtx.height = BAR_HEIGHT * 4;
+    timeSpendCtx.height = BAR_HEIGHT * 4;
+
+
   }
 }
